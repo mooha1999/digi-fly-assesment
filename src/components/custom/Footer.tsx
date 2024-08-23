5c3075ab988da00dde48602b7b979cab1800c08f
mooha1999 <developer.ahmed99@gmail.com>
2024-08-23 15:35:57 +0300

    Created Footer component

---
+++ src/components/custom/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-custom-purple text-white p-4 text-center text-sm">
+      <p>All Rights Reserved &copy; Digifly 2024</p>
+    </footer>
+  );
+}
